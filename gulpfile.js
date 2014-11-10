1b634cfd666151a565ccec1aac471c4ccdf43165
Patrick Kernke <patrick@kernke.net>
2014-11-10 13:00:36 +0100

    BrowserSync, SASS & Styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
 var gulp = require('gulp');
 var inliner = require('gulp-inline-css');
+var sass = require('gulp-ruby-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// Static server
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: "./"
+		}
+	});
+});
+
+gulp.task('sass', function() {
+	return gulp.src('sass/main.scss')
+			.pipe(sass())
+			.on('error', function (err) {
+				console.log(err.message);
+			})
+			.pipe(gulp.dest('css'));
+});
 
 gulp.task('inline', function() {
 	return gulp.src('*.html')
@@ -8,13 +29,15 @@ gulp.task('inline', function() {
 						'extraCSS' : 'css/main.css'
 					})
 				)
-				.pipe(gulp.dest('build/'));
+				.pipe(gulp.dest('build/'))
+				.pipe(reload({stream:true}));
 });
 
 gulp.task('watcher', function() {
 	gulp.watch('*.html', ['inline']);
+	gulp.watch('sass/main.scss', ['sass']);
 	gulp.watch('css/main.css', ['inline']);
 });
 
-gulp.task('default', ['inline', 'watcher']);
+gulp.task('default', ['sass', 'inline', 'watcher', 'browser-sync']);
 
